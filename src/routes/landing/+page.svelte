4408c5c884fea782c9200346c8c4b963da44edff
Travis Thurber <thurber@users.noreply.github.com>
2025-01-30 09:05:16 -0800

    initial landing

---
+++ src/routes/landing/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+
+    const msdNavItems = [
+        'About',
+        'Events',
+        'Policies',
+        'Metrics',
+        'Acknowledgement',
+    ];
+
+</script>
+
+<div
+    class="w-full flex flex-row items-center justify-center px-4 py-2 bg-rose-700 rounded-b-sm shadow"
+>
+    <span
+        class="text-sm text-white"
+    >
+        This is just a DEMO, meant to help generate ideas on what you actually want!
+    </span>
+</div>
+
+<div
+    class="
+        w-full px-8 py-2 bg-white
+        flex flex-row items-center
+    "
+>
+    <div class="h-8 overflow-hidden">
+        <enhanced:img
+            src="$lib/img/MSD.LIVE.horiz.svg"
+            alt='MSDLIVE'
+            class="h-full object-contain"
+        />
+    </div>
+</div>
